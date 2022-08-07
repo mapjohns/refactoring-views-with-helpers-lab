8ecec1d9f6da1508578a30ab1ee1ea27fdbd5dea
Matthew <mattj20@gmail.com>
2022-08-07 07:06:48 -0700

    commit tree 6c14e15abb3fcd906df6cd352e2008b3ede44372

--- app/models/song.rb
+++ app/models/song.rb
@@ -1,9 +1,13 @@
 class Song < ActiveRecord::Base
   belongs_to :artist
-
-  def artist_name
-  end
+  
 
   def artist_name=(name)
+    artist = Artist.find_or_create_by(name: name) unless name == ""
+    self.artist = artist
+  end
+
+  def artist_name
+    self.artist ? self.artist.name : nil
   end
 end
